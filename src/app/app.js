ca3117e10a2c8500f5b91a82e117b99943730c54
Michael CORNILLON <contact@dwaps.fr>
2021-02-02 19:14:19 +0100

    4ème Live Studi: récupération des vues et correction de bugs

--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,23 @@ export class App {
     this.init();
   }
   
-  init() {
-    
+  async init() {
+    await fetch('/src/views/header.html')
+      .then(res => res.text())
+      .then(data => {
+        document.body.innerHTML += data;
+      });
+
+    await fetch('/src/views/admin.html')
+      .then(res => res.text())
+      .then(data => {
+        document.body.innerHTML += data;
+      });
+
+    await fetch('/src/views/footer.html')
+      .then(res => res.text())
+      .then(data => {
+        document.body.innerHTML += data;
+      });
   }
 }
